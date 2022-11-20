4499994da1b513a352dde3414611c0f4dae5a46d
itachiuchihadev <itachi0uchiha19@gmail.com>
2022-11-20 09:49:06 +0530

    F-12323 New Message

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export class AuthService{
     
     static authenticate = async (userName, password) => {
+        
         // return await axios.post("/api/authenticate", { data: userName, password});
         return await Promise.resolve({data : { isAuthenticated: true, token : "sjflksjdfklsj32423" }});
     }
